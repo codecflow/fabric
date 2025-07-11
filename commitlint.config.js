5c4dd9c16cabaa71c10ca17990dc40909a371619
dockninja <TonyNorman22@proton.me>
2025-07-12 00:23:22 +0100

    feat(CF-4): add semantic release and commit lint configuration (#5)
    ## Motivation
    
    Implementation of #4 
    
    ## Change
    
    - added semantic release setup and configured pipeline to check and release.
    -

---
+++ commitlint.config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // only these types allowed
+    'type-enum': [
+      2, 'always',
+      [
+        'feat',
+        'fix',
+        'perf',
+        'refactor',
+        'test',
+        'revert',
+        'chore',
+        'docs',
+        'content',
+        'build',
+        'ci',
+        'hotfix',
+        'bugfix',
+        'release'
+      ]
+    ],
+    // require a scope (optional—remove if you don’t want to enforce)
+    'scope-empty': [2, 'never'],
+    // allow any subject-case
+    'subject-case': [0]
+  },
+  ignores: [
+    (message) => message.includes('Draft:')
+  ]
+
+};
