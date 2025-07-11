a9787ab939211d9a8ea35f1bf5101808112e85c4
dockninja <wm@dock.ninja>
2025-07-11 22:21:20 +0100

    feat(CF-4): add semantic release and commit lint configuration

--- commitlint.config.js
+++ commitlint.config.js
@@ -25,5 +25,9 @@ module.exports = {
     'scope-empty': [2, 'never'],
     // allow any subject-case
     'subject-case': [0]
-  }
+  },
+  ignores: [
+    (message) => message.includes('Draft:')
+  ]
+
 };
